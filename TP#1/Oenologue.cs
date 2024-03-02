9e059a0c5fd59abcd4ea6030c09d760c8168e332
kayembajoel92@gmail.com <kayembajoel92@gmail.com>
2024-03-02 16:53:23 -0500

    Ajouit oenologue

--- TP#1/Oenologue.cs
+++ TP#1/Oenologue.cs
@@ -6,7 +6,24 @@ using System.Threading.Tasks;
 
 namespace TP_1
 {
-    internal class Oenologue
+    internal class Oenologue : Client
     {
+        private Vin vin;
+        public override void Afficher()
+        {
+            Console.WriteLine("Nom: " + Nom + "\nPrenom: " + Prenom + "\nAge: " + Age);
+
+        }
+
+        public void EvaluerQualitéVin()
+        {
+            if (vin == null)
+            {
+                Console.WriteLine("Aucun vin n'est associé");
+
+            }
+
+        }
+
     }
 }
